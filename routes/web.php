ff5ad97cb3284711ac54d5eb46f5833ff9616b07
Ghazy-Othman <ghazy.h.othman@gmail.com>
2025-01-30 15:01:12 +0300

    change main route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1>
+API for Maria Hony Company
+</h1>
+<br>
+<h1>
+Developed By : Ghazy 👾
+</h1>";
 });
