cd7b495ada3cbe7b84467fc19ff8a1447925a151
Ghazy-Othman <ghazy.h.othman@gmail.com>
2025-01-31 12:23:43 +0300

    add web docs url

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/public_docs', function () {
+    return view('scribe.index');
+});
+
 Route::get('/', function () {
     return "<h1>
 API for Maria Honey Company
